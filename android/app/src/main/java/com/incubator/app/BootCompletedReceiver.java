6cb3fd2128a69cb44d6fa1830caa2de61f704acf
Tamilgamin <110589582+Tamilgamin@users.noreply.github.com>
2026-02-24 21:28:29 +0530

    Create BootCompletedReceiver.java

---
+++ android/app/src/main/java/com/incubator/app/BootCompletedReceiver.java
@@ -0,0 +1,17 @@
+package com.incubator.app;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            Intent launchIntent = context.getPackageManager()
+                    .getLaunchIntentForPackage(context.getPackageName());
+            launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(launchIntent);
+        }
+    }
+}
