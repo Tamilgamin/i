1a0ad4ce9608cdc2a0d445eae10a705bdf558761
Tamilgamin <110589582+Tamilgamin@users.noreply.github.com>
2026-02-24 21:05:11 +0530

    Add files via upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.incubator.app',
   appName: 'Incubator Monitor',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    LocalNotifications: {
+      smallIcon: "ic_stat_icon_config_sample",
+      sound: "beep.wav"
+    }
+  }
 };
 
 export default config;
